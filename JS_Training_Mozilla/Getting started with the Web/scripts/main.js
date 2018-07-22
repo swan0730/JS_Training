374a06dca5a0b54c64440f7806fdbde028b58f3a
Supun Wanniarachchi <swan0730@sysco.com>
2018-07-23 02:51:29 +0530

    Getting started with the Web exercise

---
+++ JS_Training_Mozilla/Getting started with the Web/scripts/main.js
@@ -0,0 +1,10 @@
+var myVariable = document.querySelector('h1');
+alert('yes!!!!');
+
+function multiply (num1, num2) {
+    var result =  num1*num2;
+    return result;
+}
+
+alert(multiply(10,20));
+alert(multiply(2,4));
